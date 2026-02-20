368d00fdecac6e822d37f5772668e6d28a291b1a
Ganapathi1618 <109615505+Ganapathi1618@users.noreply.github.com>
2026-02-20 05:36:03 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,12 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { supabase } from "@/lib/supabase"
 
 export default function Dashboard() {
   const router = useRouter()
+  const [userEmail, setUserEmail] = useState("")
 
   useEffect(() => {
     const checkUser = async () => {
@@ -13,15 +14,38 @@ export default function Dashboard() {
 
       if (!data.user) {
         router.push("/login")
+      } else {
+        setUserEmail(data.user.email)
       }
     }
 
     checkUser()
   }, [])
 
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+    router.push("/login")
+  }
+
   return (
     <div style={{ padding: "40px" }}>
       <h1>Welcome to Fastrill Dashboard 🚀</h1>
+      <p>Logged in as: {userEmail}</p>
+
+      <button
+        onClick={handleLogout}
+        style={{
+          marginTop: "20px",
+          padding: "10px 15px",
+          backgroundColor: "#111827",
+          color: "white",
+          border: "none",
+          borderRadius: "6px",
+          cursor: "pointer"
+        }}
+      >
+        Logout
+      </button>
     </div>
   )
 }
