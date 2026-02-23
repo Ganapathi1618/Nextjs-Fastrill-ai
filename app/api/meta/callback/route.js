046026c3d9165aff8a0b9152c80ece1e5ae354dd
Ganapathi1618 <109615505+Ganapathi1618@users.noreply.github.com>
2026-02-23 20:14:32 +0530

    Create route.js

---
+++ app/api/meta/callback/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server"
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get("code")
+
+  if (!code) {
+    return NextResponse.json({ error: "No code received" })
+  }
+
+  const appId = process.env.META_APP_ID
+  const appSecret = process.env.META_APP_SECRET
+
+  const redirectUri =
+    "https://fastrill.com/api/meta/callback"
+
+  const tokenUrl =
+    `https://graph.facebook.com/v18.0/oauth/access_token` +
+    `?client_id=${appId}` +
+    `&redirect_uri=${redirectUri}` +
+    `&client_secret=${appSecret}` +
+    `&code=${code}`
+
+  const response = await fetch(tokenUrl)
+  const data = await response.json()
+
+  return NextResponse.json(data)
+}
