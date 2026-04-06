96d0cc54c9924a5ef7ebb504b3dbd211c55ccf9e
Ganapathi1618 <109615505+Ganapathi1618@users.noreply.github.com>
2026-04-06 13:30:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@': '.',
-      },
-    },
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
